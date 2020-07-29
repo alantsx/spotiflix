f4224239d81a2e8016d0e182ce81d7c0e45ee806
Alan Pereira <alanpfabricio@gmail.com>
2020-07-29 18:13:19 -0300

    v0.42

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,22 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria() {
+    const [categorias, setCategorias] = useState([]);
+
+    const valoresIniciais = {
+        nome: 'Categoria Inicial',
+        descricao: 'Descrição Inicial',
+        cor: '#000',
+    }
+
+    const [values, setValues] = useState(valoresIniciais);
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {values.nome}</h1>
 
-            <form>
-                <label>
-                    Nome da Categoria:
-                    <input type="text"/>
-                </label>
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([...categorias, setValues]);
+            }}>
+                <div>
+                    <label>
+                        Nome da Categoria:
+                        <input
+                            type="text"
+                            value={values.nome}
+                            onChange={function handleCategoria(props) {
+                                // setValues(props.target.value);
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Descrição:
+                        <textarea
+                            type="text"
+                            value={values.descricao}
+                            onChange={function handleCategoria(props) {
+                                // setValues(props.target.value);
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            value={values.cor}
+                            onChange={function handleCategoria(props) {
+                                // setNomeDaCategoria(props.target.value);
+                            }}
+                        />
+                    </label>
+                </div>
 
                 <button>
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, index) => {
+                    return (
+                        <li key={`${categoria}${index}`}>
+                            {categoria}
+                        </li>
+                    );
+                })}
+            </ul>     
             
             <Link to="/">
                 Cadastrar Categoria
